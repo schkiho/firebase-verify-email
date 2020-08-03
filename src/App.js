66c476cfc0e498399c1c7d6663763a57b06e870e
schkiho <schkiho@yahoo.de>
2020-08-03 13:37:37 +0200

    add materialize & setup firebase config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default App;
