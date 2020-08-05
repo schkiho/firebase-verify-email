814f9e38d54e0abbcfb6f19a5f4b4884310bee4d
schkiho <schkiho@yahoo.de>
2020-08-05 11:46:28 +0200

    set auth-listener with context API & protect dashboard route

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ import Home from "./components/Home";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import { AuthProvider } from "./components/auth/Auth";
+import PrivateRoute from "./components/auth/PrivateRoute";
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/dashboard" component={Dashboard} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 };
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
+import app from "../firebase";
 
 const Home = () => {
+  console.log(app.auth().currentUser);
   return (
     <div className="container">
       <h1 className="text-center">Home</h1>
