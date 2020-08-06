b4bd2c4be3da9fd5a0f33d91fb4b3cfae8919988
schkiho <schkiho@yahoo.de>
2020-08-06 12:45:57 +0200

    switch back use self created modal in login for verification

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,59 +1,78 @@
-import React from "react";
+import React, { Fragment, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import { useForm } from "react-hook-form";
 import { joiResolver } from "@hookform/resolvers";
 
 import { loginSchema } from "../validationSchemas/authSchemas";
-import { loginUser } from "../auth/authFunctions";
+import { loginUser } from "./authFunctions";
+import { AuthContext } from "./Auth";
+import Modal from "./VerifyModal";
 
 const Login = ({ history }) => {
+  const { currentUser } = useContext(AuthContext);
+  const [show, setShow] = useState(false);
+
   const { register, handleSubmit, errors } = useForm({
     resolver: joiResolver(loginSchema),
   });
 
   const onSubmit = (data) => {
     loginUser(data);
-    history.push("/dashboard");
+    if (currentUser) {
+      currentUser.emailVerified ? history.push("/dashboard") : showModal();
+    }
   };
 
+  const showModal = () => setShow(true);
+
+  const hideModal = () => setShow(false);
+
   return (
-    <div className="container auth-container my-4">
-      <h2 className="text-center text-success">
-        <i className="fa fa-user" /> Login
-      </h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-group">
-          <input
-            type="email"
-            className="form-control"
-            ref={register}
-            name="email"
-            placeholder="Email address"
-          />
-          <p className="text-center text-danger">{errors.email?.message}</p>
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            className="form-control"
-            ref={register}
-            name="password"
-            placeholder="Password"
-          />
-          <p className="text-center text-danger">{errors.password?.message}</p>
-        </div>
-        <div className="fourm-group">
-          <button type="submit" className="btn btn-success btn-block">
-            Login
-          </button>
-        </div>
-      </form>
-      <hr />
-      <p className="text-center">
-        You don't have an account <Link to="/register">Register</Link>.
-      </p>
-    </div>
+    <Fragment>
+      <div className="container auth-container my-4">
+        <h2 className="text-center text-success">
+          <i className="fa fa-user" /> Login
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              ref={register}
+              name="email"
+              placeholder="Email address"
+            />
+            <p className="text-center text-danger">{errors.email?.message}</p>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              ref={register}
+              name="password"
+              placeholder="Password"
+            />
+            <p className="text-center text-danger">
+              {errors.password?.message}
+            </p>
+          </div>
+          <div className="fourm-group">
+            <button type="submit" className="btn btn-success btn-block">
+              Login
+            </button>
+          </div>
+        </form>
+        <hr />
+        <p className="text-center">
+          You don't have an account <Link to="/register">Register</Link>.
+        </p>
+      </div>
+      <Modal show={show} handleClose={hideModal}>
+        <p>Modal</p>
+        <p>Data</p>
+      </Modal>
+    </Fragment>
   );
 };
 
